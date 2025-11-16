0cba57775a443c50c8d348220e26e59f2dc679ed
Yojan Contreras <yojanarley159@gmail.com>
2025-11-15 21:33:52 -0500

    Create config.py

---
+++ Lab6/Juego/config.py
@@ -0,0 +1,8 @@
+                                                                                              config.py                                                                                                                 
+WIDTH = 800
+HEIGHT = 600
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
